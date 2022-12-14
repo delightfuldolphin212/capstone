ee20b5f8254479f91002ccd5b4a873c96f7ef668
Zachary Rosenfeld <zachary_rosenfeld1+cs1300@brown.edu>
2022-12-14 08:40:54 -0500

    cart table format

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,30 @@
 import CartItem from "../components/CartItem";
+import Table from "react-bootstrap/Table";
 export default function Cart(props) {
   return (
-    <div>
+    <div className="cart-container">
       <h1 className="total-price">Total Price: ${props.price}</h1>
-      <div className="half-container">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.cartItems.map((item, index) => (
+            <tr>
+              <td>
+                {item.year} {item.name}
+              </td>
+              <td>{item.size}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {/* <div className="cart-container">
         {props.cartItems.map((item, index) => (
           <CartItem
             item={item}
@@ -14,7 +35,7 @@ export default function Cart(props) {
             setPrice={props.setPrice}
           />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
